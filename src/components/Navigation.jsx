9841028bdaa8369b7a66d794bda28188a06aa8b5
ikurowski <igorkurowski@gmail.com>
2022-04-04 17:16:33 +0200

    add signIn buttons

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,12 +1,17 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import Link from 'react-scroll/modules/components/Link';
+import { Link as RouterLink } from 'react-router-dom';
 
 export default function Navigation() {
   return (
-    <nav>
-      <ul className="navigation">
-        {/*  FIXME Czy to jest odpowiednie miejsce i nazwa dla klasy w bem */}
+    <nav className="navigation">
+      <ul className="navigation__menu container">
+        <li><RouterLink to="/" className="btn--login">Zaloguj</RouterLink></li>
+        <li><RouterLink to="/" className="btn--register">Załóż konto</RouterLink></li>
+      </ul>
+      {/* FIXME Dlaczego na te linki nie działa przełączanie tab? */}
+      <ul className="navigation__menu container">
         <li><Link to="" smooth duration={500} className="btn">Start</Link></li>
         <li><Link to="" smooth duration={500} className="btn">O co chodzi?</Link></li>
         <li><Link to="" smooth duration={500} className="btn">O nas</Link></li>
