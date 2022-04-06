1de9c5ca0f5e048b5c6e8f778089cb3641c38464
ikurowski <igorkurowski@gmail.com>
2022-04-07 00:19:51 +0200

    HomeSimpleSteps done

--- src/components/Home/HomeSimpleSteps.jsx
+++ src/components/Home/HomeSimpleSteps.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import DecorationTitle from '../DecorationTitle';
 
 export default function HomeSimpleSteps() {
   return (
-    <>
-
-    </>
+    <section className="simple-steps">
+      <DecorationTitle firstText="Wystarczą 4 proste kroki" />
+    </section>
   );
 }
