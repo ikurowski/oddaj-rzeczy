e69afec52161fb6f746b9f6b391b326717be720e
ikurowski <igorkurowski@gmail.com>
2022-04-07 00:14:46 +0200

    add prop types to buttonLarge

--- src/components/ButtonLarge.jsx
+++ src/components/ButtonLarge.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-export default function ButtonLarge() {
+export default function ButtonLarge({ text = '' }) {
   return (
     <Link className="btn--large" to="rejestracja">
-      ODDAJ RZECZY
+      {text}
     </Link>
   );
 }
+
+ButtonLarge.propTypes = {
+  text: PropTypes.string.isRequired,
+};
