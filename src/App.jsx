24d92bd27ce165eb8304693373f4a4ab740f463c
ikurowski <igorkurowski@gmail.com>
2022-04-20 10:29:17 +0200

    delete ErrorPage component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
 import SignUp from './components/SignUp';
 import Login from './components/Login';
-import ErrorPage from './components/ErrorPage';
 import Logout from './components/Logout';
 import Navigation from './components/Navigation';
 
@@ -16,7 +15,6 @@ function App() {
         <Route path="rejestracja" element={<SignUp />} />
         <Route path="logowanie" element={<Login />} />
         <Route path="wylogowano" element={<Logout />} />
-        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </Router>
   );
